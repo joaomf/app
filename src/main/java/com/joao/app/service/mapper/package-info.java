ca8bcbef82f9e42e52eeadcfd75c5f6974eb32fe
Joao Melo Ferreira <joaomeloferreira@gmail.com>
2020-02-15 11:00:28 -0300

    Initial version of app generated by JHipster-6.7.1

---
+++ src/main/java/com/joao/app/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.joao.app.service.mapper;
---
+++ src/main/java/com/joao/app/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Service layer beans.
+ */
+package com.joao.app.service;
